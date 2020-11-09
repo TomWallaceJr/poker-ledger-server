8fc08f921f681eca3804c1f9ca3eb0191acaf4fa
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2020-11-09 17:13:04 -0500

    knex installed

--- src/app.js
+++ src/app.js
@@ -15,10 +15,14 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello, boilerplate!');
 });
 
+// error handling middleware
 app.use(function errorHandler(error, req, res, next) {
     let response;
     if (NODE_ENV === 'production') {
--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
+const knex = require('knex');
 const app = require('./app');
 
-const { PORT } = require('./config');
+const { PORT, DB_URL } = require('./config');
+
+// create knex object to communicate with DB and set object to 'db' variable
+const db = knex({
+    client: 'pg',
+    connection: DB_URL,
+})
+app.set('db', db)
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
